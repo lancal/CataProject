531f9926411612629caed1aac290c2fa3f7c3f3a
alan <a.calderon07@hotmail.com>
2023-10-04 10:08:21 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def area_rec (number1, number2):
+
+    return number1 * number2
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(area_rec(10,2))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
